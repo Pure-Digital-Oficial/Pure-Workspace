67a34f9c44addd0de1380e35371e2d0e95fcb21e
Erick Celestino <erickcelestimo@gmail.com>
2025-01-09 11:25:16 -0300

    feat: add list medias post repository implementation

--- libs/domain/src/lib/dto/request/blog/post/media/index.ts
+++ libs/domain/src/lib/dto/request/blog/post/media/index.ts
@@ -1,4 +1,5 @@
 export * from './create-media-post.dto';
 export * from './create-medias-post.dto';
 export * from './list-medias-post.dto';
+export * from './media-post-prisma.dto';
 export * from './media-post.dto';
