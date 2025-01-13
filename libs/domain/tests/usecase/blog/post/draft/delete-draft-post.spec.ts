e11e877c9d2feadf20e759e5c2b36c146b05ee3e
Erick Celestino <erickcelestimo@gmail.com>
2025-01-13 09:15:10 -0300

    test: ensure user inputs validation in delete draft post use case

---
+++ libs/domain/tests/usecase/blog/post/draft/delete-draft-post.spec.ts
@@ -0,0 +1,119 @@
+import {
+  DeleteDraftPost,
+  DeleteDraftPostDto,
+  DeleteDraftPostRepository,
+  EntityNotDeleted,
+  EntityNotEmpty,
+  EntityNotExists,
+  FindPostByIdRepository,
+  FindUserByIdRepository,
+  PostResponseDto,
+  UserList,
+} from '../../../../../src';
+import { PostMock, userMock } from '../../../../entity';
+import {
+  DeleteDraftPostRepositoryMock,
+  FindPostByIdRepositoryMock,
+  FindUserByIdRepositoryMock,
+} from '../../../../repository';
+
+interface SutTypes {
+  sut: DeleteDraftPost;
+  deletePostDto: DeleteDraftPostDto;
+  findUserByIdRepository: FindUserByIdRepository;
+  findPostByIdRepository: FindPostByIdRepository;
+  deleteDraftPostRepository: DeleteDraftPostRepository;
+}
+
+const makeSut = (): SutTypes => {
+  const findUserByIdRepository = new FindUserByIdRepositoryMock();
+  const findPostByIdRepository = new FindPostByIdRepositoryMock();
+  const deleteDraftPostRepository = new DeleteDraftPostRepositoryMock();
+
+  const deletePostDto: DeleteDraftPostDto = {
+    id: PostMock.id,
+    loggedUserId: userMock.userId,
+  };
+
+  const sut = new DeleteDraftPost(
+    findUserByIdRepository,
+    findPostByIdRepository,
+    deleteDraftPostRepository
+  );
+
+  return {
+    sut,
+    deletePostDto,
+    findUserByIdRepository,
+    findPostByIdRepository,
+    deleteDraftPostRepository,
+  };
+};
+
+describe('DeleteDraftPost', () => {
+  it('should return Post ID when pass correct deletePostDto', async () => {
+    const { deletePostDto, sut } = makeSut();
+
+    const result = await sut.execute(deletePostDto);
+
+    expect(result.isLeft()).toBeFalsy();
+    expect(result.isRight()).toBeTruthy();
+    expect(result.value).toStrictEqual(PostMock.id);
+  });
+
+  it('should return EntityNotEmpty when pass empty user ID in deletePostDto', async () => {
+    const { deletePostDto, sut } = makeSut();
+    deletePostDto.loggedUserId = '';
+    const result = await sut.execute(deletePostDto);
+
+    expect(result.isLeft()).toBeTruthy();
+    expect(result.isRight()).toBeFalsy();
+    expect(result.value).toBeInstanceOf(EntityNotEmpty);
+  });
+
+  it('should return EntityNotEmpty when pass empty post ID in deletePostDto', async () => {
+    const { deletePostDto, sut } = makeSut();
+    deletePostDto.id = '';
+    const result = await sut.execute(deletePostDto);
+
+    expect(result.isLeft()).toBeTruthy();
+    expect(result.isRight()).toBeFalsy();
+    expect(result.value).toBeInstanceOf(EntityNotEmpty);
+  });
+
+  it('should return EntityNotExists when a exist User in system', async () => {
+    const { deletePostDto, sut } = makeSut();
+    jest
+      .spyOn(sut['findUserByIdRepository'], 'find')
+      .mockResolvedValueOnce({} as UserList);
+    const result = await sut.execute(deletePostDto);
+
+    expect(result.isLeft()).toBe(true);
+    expect(result.isRight()).toBe(false);
+    expect(result.value).toBeInstanceOf(EntityNotExists);
+  });
+
+  it('should return EntityNotExists when a exist Post in system', async () => {
+    const { deletePostDto, sut } = makeSut();
+    jest
+      .spyOn(sut['findPostByIdRepository'], 'find')
+      .mockResolvedValueOnce({} as PostResponseDto);
+    const result = await sut.execute(deletePostDto);
+
+    expect(result.isLeft()).toBe(true);
+    expect(result.isRight()).toBe(false);
+    expect(result.value).toBeInstanceOf(EntityNotExists);
+  });
+
+  it('should return EntityNotDeleted when a not delete Post in system', async () => {
+    const { deletePostDto, sut } = makeSut();
+    jest
+      .spyOn(sut['deleteDraftPostRepository'], 'delete')
+      .mockResolvedValueOnce('');
+    const result = await sut.execute(deletePostDto);
+
+    expect(result.isLeft()).toBe(true);
+    expect(result.isRight()).toBe(false);
+    expect(result.value).toBeInstanceOf(EntityNotDeleted);
+  });
+});
