e2679c1a2eee3eaac702e345cd1ddcdc9013daa9
Erick Celestino <erickcelestimo@gmail.com>
2025-02-05 11:15:15 -0300

    refactor: ajust prisma schema production and ajust app, main components

--- apps/web/web-apol-blog/src/main.tsx
+++ apps/web/web-apol-blog/src/main.tsx
@@ -4,8 +4,9 @@ import { BrowserRouter } from 'react-router-dom';
 
 import App from './app/app';
 import { AppIdProvider } from '@pure-workspace/feature';
+import { env } from './app/env';
 
-const appId = process.env['NX_PUBLIC_APOL_BLOG_ID'] ?? '';
+const appId = env.NX_PUBLIC_APOL_BLOG_ID;
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
