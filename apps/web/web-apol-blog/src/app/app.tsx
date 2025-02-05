8181e7e04d7309ea65f980ef0443fdf73bcbd542
Erick Celestino <erickcelestimo@gmail.com>
2025-02-05 10:29:11 -0300

    refactor: ajust dockerfile from web apol blog

--- apps/web/web-apol-blog/src/app/app.tsx
+++ apps/web/web-apol-blog/src/app/app.tsx
@@ -1,8 +1,9 @@
 import { ContentApp } from './content-app';
-import { env } from './env';
 
 export function App() {
-  return <ContentApp clientId={env.NX_PUBLIC_EXTERNAL_CLIENT_ID} />;
+  return (
+    <ContentApp clientId={process.env['NX_PUBLIC_EXTERNAL_CLIENT_ID'] ?? ''} />
+  );
 }
 
 export default App;
